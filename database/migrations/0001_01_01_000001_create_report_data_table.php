57373921a2f9be38d71440f007fcfee03f0c59cc
2497815159 <anastasius.manurung@bankmandiri.co.id>
2024-12-24 03:39:44 +0700

    add_encrypt_and_decrypt

--- database/migrations/0001_01_01_000001_create_report_data_table.php
+++ database/migrations/0001_01_01_000001_create_report_data_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('filename')->unique();
             $table->string('extension');
             $table->text('data');
+            $table->binary('data_binary');
             $table->timestamps();
         });
     }
